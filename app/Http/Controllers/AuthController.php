c64ea1c235a098125f569cb1a9852ba420ed6cf7
[AndreaS] <solfa_90@hotmail.it>
2021-06-04 16:29:25 +0200

    register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Hash;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(RegisterRequest $request)
     {
-        return 'hello';
+        if ($request->has(['first_name', 'last_name', 'email'])) {
+            $user = User::create(
+                $request->only('first_name', 'last_name', 'email')
+                    + [
+                        'password' => Hash::make($request->input('password')),
+                        'is_admin' => 1,
+                    ]
+            );
+        }
+
+        return response($user, Response::HTTP_CREATED);
     }
 }
