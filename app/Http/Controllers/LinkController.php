a40b77fd7d87a2f5b9ccadd4fcaec1999d7fae36
[AndreaS] <solfa_90@hotmail.it>
2021-06-07 17:46:52 +0200

    links controller, route, model exc...

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Link;
+
+class LinkController extends Controller
+{
+    public function index($id)
+    {
+        return Link::where('user_id', $id)->get();
+    }
+}
